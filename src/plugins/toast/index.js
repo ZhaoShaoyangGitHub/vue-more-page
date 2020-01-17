da43e6519dd37dd8d3447a226d899275d1229686
ee <shaoyang.zhao@dookay.com>
2020-01-17 13:55:14 +0800

    小修改

--- src/plugins/toast/index.js
+++ src/plugins/toast/index.js
@@ -1,11 +1,11 @@
 import toastcom from './toast.vue'
 var toast = {}
-toast.install = vue => {
-    let ToastCon = vue.extend(toastcom)
+toast.install = Vue => {
+    let ToastCon = Vue.extend(toastcom)
     let ins = new ToastCon()
     ins.$mount(document.createElement('div'))  // 挂载到一个元素上
     document.body.appendChild(ins.$el)
-    vue.prototype.$toast = ({text, duration=3000}) => {
+    Vue.prototype.$toast = ({text, duration=3000}) => {
         ins.text = text
         ins.show = true
         let tiemr = setTimeout(() => {
